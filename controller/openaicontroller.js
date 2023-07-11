a054b7460b28415564f258c7186dd185ba4e61c9
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-11 22:34:58 +0300

    Common GPT

--- controller/openaicontroller.js
+++ controller/openaicontroller.js
@@ -1,1 +1,16 @@
-const openai = require('../config/')
+const openai = require('../controller/openaiconfig')
+
+const generateMeta = async(title) =>{
+    const description = await openai.createChatCompletion({
+        model:"gpt-3-5-turbo",
+        messages:[
+            {
+role: 'user',
+content:'Come up with a description for your youtube video called ${title}'
+            }
+        ],
+        max_tokens: 100
+    })
+    console.log(description.data.choices[0].message)
+}
+module.exports={generateMeta}
