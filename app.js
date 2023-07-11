6e35dcb2bf03e439ef66ea14d5da5a7e2f3c5429
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-11 22:23:09 +0300

    Common GPT

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-console.log('hello mehn');
 const readline = require('readline');
+const{ generateMeta } = require('./controller/openaicontroller')
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question('Youtube Video Title\n', (title) => {
-    console.log(title);
+rl.question('Youtube Video Title\n', generateMeta) 
     // rl.close(); // Close the readline interface
-});
