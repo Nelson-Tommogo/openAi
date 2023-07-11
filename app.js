53f2b256fc558c3e530bc477c472a51946333ddf
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-11 22:05:16 +0300

    Common GPT

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello mehn')
+console.log('hello mehn');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Youtube Video Title\n', (title) => {
+    console.log(title);
+    // rl.close(); // Close the readline interface
+});
